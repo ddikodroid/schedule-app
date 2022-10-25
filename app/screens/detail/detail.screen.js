187b426260819f6c663b341eb0455648995cb937
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2022-10-25 18:37:05 +0700

    feat(detail-schedule): show all schedule details

--- app/screens/detail/detail.screen.js
+++ app/screens/detail/detail.screen.js
@@ -1,11 +1,30 @@
+import dayjs from 'dayjs';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet} from 'react-native';
+import {Screen, ScreenHeader, TimeInfo} from '../../components';
+import DetailClock from './detail-clock.component';
+import DetailSection from './detail-section.component';
+import StoreInfo from './store-info.component';
 
-const DetailScreen = () => {
+const DetailScreen = ({route: {params}}) => {
   return (
-    <View>
-      <Text>DetailScreen</Text>
-    </View>
+    <Screen>
+      <ScreenHeader title={dayjs(params.date).format('D MMMM YYYY')} />
+      <ScrollView bounces={false}>
+        <DetailSection title="Store">
+          <StoreInfo {...params} />
+        </DetailSection>
+        <DetailSection title="Time Schedule">
+          <TimeInfo startTime={params.startTime} endTime={params.endTime} />
+        </DetailSection>
+        <DetailSection title="Clock In">
+          <DetailClock time={params.clockIn} />
+        </DetailSection>
+        <DetailSection title="Clock Out">
+          <DetailClock time={params.clockOut} />
+        </DetailSection>
+      </ScrollView>
+    </Screen>
   );
 };
 
