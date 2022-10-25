6d97a73ea6af19637891e7d046196b4f8cbbe64f
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2022-10-25 15:48:46 +0700

    feat(home): clock in and out button

--- app/screens/home/home.screen.js
+++ app/screens/home/home.screen.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import dayjs from 'dayjs';
 import React, {useEffect, useState} from 'react';
-import {FlatList, ScrollView, StyleSheet, Text} from 'react-native';
+import {FlatList, ScrollView, StyleSheet, Text, View} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-import {Screen} from '../../components';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {Button, Screen} from '../../components';
 import {colors, fonts} from '../../styles';
 import HomeHeader from './home-header.component';
 import HomeSection from './home-section.component';
@@ -20,12 +21,11 @@ const HomeScreen = () => {
   const currentDate = dayjs().date();
 
   const navigation = useNavigation();
+  const insets = useSafeAreaInsets();
 
   const [schedules, setSchedules] = useState([]);
   const [nextSchedules, setNextSchedules] = useState([]);
   const todaySchedule = schedules?.[year]?.[month]?.[date];
-
-  console.log(nextSchedules.length);
 
   useEffect(() => {
     const getSchedules = async () => {
@@ -55,34 +55,50 @@ const HomeScreen = () => {
 
   return (
     <Screen>
-      <ScrollView bounces={false}>
-        <HomeHeader />
-        <HomeSection leftTitle="Today's Schedule" rightTitle="Refresh">
-          {todaySchedule != null ? (
-            <TodayCard
-              end={todaySchedule?.endTime}
-              start={todaySchedule?.startTime}
-              location={todaySchedule?.location}
+      <ScrollView
+        bounces={false}
+        contentContainerStyle={styles.scrollContentContainer}>
+        <View>
+          <HomeHeader />
+          <HomeSection leftTitle="Today's Schedule" rightTitle="Refresh">
+            {todaySchedule != null ? (
+              <TodayCard
+                end={todaySchedule?.endTime}
+                start={todaySchedule?.startTime}
+                location={todaySchedule?.location}
+              />
+            ) : (
+              <Text style={styles.emptyPlaceholder}>
+                You don't have any schedule today.
+              </Text>
+            )}
+          </HomeSection>
+          <HomeSection
+            leftTitle="Next Schedule"
+            rightTitle="See all"
+            onPressRightTitle={() => navigation.navigate('List')}>
+            <FlatList
+              horizontal
+              data={nextSchedules.slice(currentDate)}
+              renderItem={renderNextSchedule}
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={styles.flatListContent}
             />
-          ) : (
-            <Text style={styles.emptyPlaceholder}>
-              You don't have any schedule today.
-            </Text>
-          )}
-        </HomeSection>
-
-        <HomeSection
-          leftTitle="Next Schedule"
-          rightTitle="See all"
-          onPressRightTitle={() => navigation.navigate('List')}>
-          <FlatList
-            horizontal
-            data={nextSchedules.slice(currentDate)}
-            renderItem={renderNextSchedule}
-            showsHorizontalScrollIndicator={false}
-            contentContainerStyle={styles.flatListContent}
+          </HomeSection>
+        </View>
+        <View style={[styles.buttonContainer, {marginBottom: insets.bottom}]}>
+          <Button
+            title="Clock In"
+            type="primary"
+            containerStyle={styles.clockIn}
           />
-        </HomeSection>
+          <Button
+            title="Clock Out"
+            type="secondary"
+            disabled
+            containerStyle={styles.clockOut}
+          />
+        </View>
       </ScrollView>
     </Screen>
   );
@@ -91,6 +107,10 @@ const HomeScreen = () => {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
+  scrollContentContainer: {
+    flexGrow: 1,
+    justifyContent: 'space-between',
+  },
   emptyPlaceholder: {
     marginTop: 4,
     ...fonts.regular,
@@ -100,5 +120,17 @@ const styles = StyleSheet.create({
   flatListContent: {
     paddingEnd: 8,
     paddingStart: 16,
+  },
+  buttonContainer: {
+    marginTop: 32,
+    flexDirection: 'row',
+    paddingHorizontal: 16,
+    justifyContent: 'space-between',
+  },
+  clockIn: {
+    marginEnd: 6,
+  },
+  clockOut: {
+    marginStart: 6,
   },
 });
