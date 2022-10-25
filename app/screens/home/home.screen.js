06898bde20454a78b8a77087c2294b808ed02f19
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2022-10-25 13:46:42 +0700

    feat(home): today's schedule

--- app/screens/home/home.screen.js
+++ app/screens/home/home.screen.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import axios from 'axios';
+import dayjs from 'dayjs';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text} from 'react-native';
 import {Screen} from '../../components';
+import {colors, fonts} from '../../styles';
 import HomeHeader from './home-header.component';
 import HomeSection from './home-section.component';
+import TodayCard from './today-card.component';
 
 const HomeScreen = () => {
+  const year = dayjs().year();
+  const month = dayjs().format('YYYY-MM');
+  const date = dayjs().format('YYYY-MM-DD');
+
+  const [schedules, setSchedules] = useState([]);
+  const todaySchedule = schedules?.[year]?.[month]?.[date];
+
+  useEffect(() => {
+    const getSchedules = async () => {
+      try {
+        const res = await axios.get(
+          'https://gist.githubusercontent.com/ddikodroid/571f7a340bc5d33f22ece09b3af8738b/raw/a8b5aedc83be51afe088e7d8ccecd827bdcb1c41/schedule.json',
+        );
+        setSchedules(res.data);
+      } catch (error) {
+        console.log('[ERROR] on getting schedules: ', error);
+      }
+    };
+    getSchedules();
+  }, []);
   return (
     <Screen>
       <HomeHeader />
       <HomeSection leftTitle="Today's Schedule" rightTitle="Refresh">
-        <Text>Working in progress</Text>
+        {todaySchedule != null ? (
+          <TodayCard
+            end={todaySchedule?.endTime}
+            start={todaySchedule?.startTime}
+            location={todaySchedule?.location}
+          />
+        ) : (
+          <Text style={styles.emptyPlaceholder}>
+            You don't have any schedule today.
+          </Text>
+        )}
       </HomeSection>
+
       <HomeSection leftTitle="Next Schedule" rightTitle="See all">
         <Text>Working in progress</Text>
       </HomeSection>
@@ -20,4 +55,11 @@ const HomeScreen = () => {
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  emptyPlaceholder: {
+    marginTop: 4,
+    ...fonts.regular,
+    alignSelf: 'center',
+    color: colors.black,
+  },
+});
---
+++ app/screens/home/today-card.component.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Clock} from '../../assets/svgs';
+import {colors, fonts} from '../../styles';
+import ClockComp from './clock.component';
+
+const TodayCard = ({start, end, location, clockIn = null, clockOut = null}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.locationText}>{location}</Text>
+      <View style={styles.timeContainer}>
+        <Clock width={18} height={18} />
+        <Text style={styles.timeText}>
+          {start} - {end}
+        </Text>
+      </View>
+      <View style={styles.clockContainer}>
+        <ClockComp type="in" />
+        <ClockComp type="out" />
+      </View>
+    </View>
+  );
+};
+
+export default TodayCard;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    borderRadius: 16,
+    backgroundColor: colors.lightGrey,
+  },
+  locationText: {
+    marginBottom: 6,
+    ...fonts.semiBold,
+    color: colors.black,
+    fontSize: 15,
+  },
+  timeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  timeText: {
+    fontSize: 13,
+    marginStart: 8,
+    ...fonts.regular,
+    color: colors.black,
+  },
+  clockContainer: {
+    marginTop: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  line: {
+    backgroundColor: colors.grey,
+    height: 1,
+    flex: 1,
+  },
+});
