11bddba9e19b9bd8f2ed438db51db7ed9c107c84
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2022-10-25 18:37:18 +0700

    chore: change url

--- app/constants/url.js
+++ app/constants/url.js
@@ -1,2 +1,2 @@
 export const SCHEDULE_URL =
-  'https://gist.githubusercontent.com/ddikodroid/571f7a340bc5d33f22ece09b3af8738b/raw/7e6dba6f5e39145e4ae85bf987171dbfde8c3b73/schedule.json';
+  'https://gist.githubusercontent.com/ddikodroid/571f7a340bc5d33f22ece09b3af8738b/raw/f87a172f930ebee8ebc780d5a04a494a8db9a8b5/schedule.json';
