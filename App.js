1460d302dcb5e753ee8bbfb4cd79eab993e248cb
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2022-10-25 12:10:48 +0700

    chore(android): set statusbar color to yellow

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RNBootSplash from 'react-native-bootsplash';
 import {AppRoute} from './app/routes';
+import {StatusBar} from 'react-native';
+import {colors} from './app/styles';
 
 const App = () => {
   return (
     <NavigationContainer onReady={() => RNBootSplash.hide({fade: true})}>
+      <StatusBar backgroundColor={colors.yellow} barStyle="dark-content" />
       <AppRoute />
     </NavigationContainer>
   );
