813bac5ff2e16eaf80a6231d50684adf88986686
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2022-10-25 11:38:44 +0700

    chore: add images and svgs

---
+++ app/assets/svgs/index.js
@@ -0,0 +1,5 @@
+export {default as ArrowLeft} from './arrow-left.svg';
+export {default as Bell} from './bell.svg';
+export {default as Clock} from './clock.svg';
+export {default as QR} from './qr.svg';
+export {default as Refresh} from './refresh.svg';
