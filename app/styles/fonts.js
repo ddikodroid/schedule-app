20b922eb528e12cd4674b3f692599b0431cc041c
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2022-10-25 11:02:05 +0700

    style: add font family

---
+++ app/styles/fonts.js
@@ -0,0 +1,17 @@
+export const fonts = {
+  bold: {
+    fontFamily: 'OpenSans-Bold',
+  },
+  semiBold: {
+    fontFamily: 'OpenSans-SemiBold',
+  },
+  medium: {
+    fontFamily: 'OpenSans-Medium',
+  },
+  regular: {
+    fontFamily: 'OpenSans-Regular',
+  },
+  condensedBold: {
+    fontFamily: 'OpenSansCondensed-Bold',
+  },
+};
