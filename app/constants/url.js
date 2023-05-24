62511a58720f7568dcb6e446f6925b1e69b4664a
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2023-05-24 15:57:59 +0700

    refactor: change schedule url

--- app/constants/url.js
+++ app/constants/url.js
@@ -1,4 +1,4 @@
 export const SCHEDULE_URL =
-  'https://gist.githubusercontent.com/ddikodroid/571f7a340bc5d33f22ece09b3af8738b/raw/f87a172f930ebee8ebc780d5a04a494a8db9a8b5/schedule.json';
+  'https://gist.githubusercontent.com/ddikodroid/571f7a340bc5d33f22ece09b3af8738b/raw/d43403afeb4a1ee62239f2723f74b7cf82348d5d/schedule.json';
 export const IMAGE_URL =
   'https://gist.githubusercontent.com/ddikodroid/3f8c3e6b041949e5cdc242a339124a77/raw/dc550e1e159073fa974dbbc38487c2c716a9e8e5/user.json';
